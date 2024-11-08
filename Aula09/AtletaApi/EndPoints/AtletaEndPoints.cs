39421ab2ba1a838de701c0dacc5fba6ac9b6651c
danilo <danilo.l.rezende@viannasempre.com.br>
2024-11-07 21:33:23 -0300

    semana 14

--- Aula09/AtletaApi/EndPoints/AtletaEndPoints.cs
+++ Aula09/AtletaApi/EndPoints/AtletaEndPoints.cs
@@ -1,27 +1,26 @@
 using System;
+using AtletaApi.Infra;
 using AtletaApi.Models;
+using Aula10;
 
 namespace AtletaApi.EndPoints;
 
 public static class AtletaEndPoints
 {
-    static AtletaEndPoints()
-    {
-        objetos = [];
-    }
 
     public static void AdicionarAtletaEndPoints(this WebApplication app)
     {
-        app.MapGet("/atletas", Get);
-        app.MapGet("/atletas/{id}", GetById);
-        app.MapPost("/atletas", Post);
-        app.MapPut("/atletas/{id}",Put);
-        app.MapDelete("/atletas/{id}", Delete);
+        var grupo = app.MapGroup("/atletas");
+        grupo.MapGet("/", Get);
+        grupo.MapGet("/{id}", GetById);
+        grupo.MapPost("", Post);
+        grupo.MapPut("/{id}",Put);
+        grupo.MapDelete("/{id}", Delete);
     }
 
-    private static IResult GetById(long id)
+    private static IResult GetById(long id, AtletaContext db)
     {
-        var obj = objetos.FirstOrDefault(x => x.Id == id);
+        var obj = db.Atletas.Find(id);
 
         if (obj == null)
         {
@@ -30,27 +29,28 @@ public static class AtletaEndPoints
         return TypedResults.Ok(obj);
     }
 
-    private static IResult Get()
-    {
-        return objetos != null ? TypedResults.Ok(objetos) : TypedResults.NotFound();
+    private static IResult Get(AtletaContext db)
+    {        
+        return TypedResults.Ok(db.Atletas.ToList());
     }
 
-    private static IResult Post(Atleta obj)
+    private static IResult Post(Atleta obj, AtletaContext db)
     {
-        obj.Id = (objetos.MaxBy(x => x.Id) ?? new Atleta()).Id + 1;
-        objetos.Add(obj);
+        obj.Id = GeradorId.GetId();
+        db.Atletas.Add(obj);
+        db.SaveChanges();
 
         return TypedResults.Created($"atletas/{obj.Id}", obj);
     }
 
-    private static IResult Put(long id, Atleta objNovo)
+    private static IResult Put(long id, Atleta objNovo, AtletaContext db)
     {
         if (id != objNovo.Id)
         {
             return TypedResults.BadRequest();
         }
 
-        var obj = objetos.FirstOrDefault(x => x.Id == id);
+        var obj = db.Atletas.Find(id);
 
         if (obj == null)
         {
@@ -61,23 +61,25 @@ public static class AtletaEndPoints
         obj.Altura = objNovo.Altura;
         obj.Peso = objNovo.Peso;
 
+        db.Atletas.Update(obj);
+        db.SaveChanges();
+
         return TypedResults.NoContent();
     }
 
-    private static IResult Delete(long id)
+    private static IResult Delete(long id, AtletaContext db)
     {
-        var obj = objetos.FirstOrDefault(x => x.Id == id);
+        var obj = db.Atletas.Find(id);
 
         if (obj == null)
         {
             return TypedResults.NotFound();
         }
 
-        objetos.Remove(obj);
+        db.Atletas.Remove(obj);
+        db.SaveChanges();   
 
         return TypedResults.NoContent();
     }
 
-
-    private static readonly IList<Atleta> objetos;
 }
